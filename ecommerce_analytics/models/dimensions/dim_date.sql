89d5f607de1b27c9835f1903d45fc71120d9d6e8
Vincius <viniviniee11@gmail.com>
2026-06-06 15:19:26 -0300

    feat: faz a conversão de tipos para data dos campos de data da tabela

--- ecommerce_analytics/models/dimensions/dim_date.sql
+++ ecommerce_analytics/models/dimensions/dim_date.sql
@@ -1,11 +1,11 @@
 {{ config(materialized='table') }}
 
 SELECT DISTINCT
-    toDate(parseDateTimeBestEffort(purchased_at)) AS date_id,
-    toYear(parseDateTimeBestEffort(purchased_at)) AS year,
-    toMonth(parseDateTimeBestEffort(purchased_at)) AS month,
-    toDayOfMonth(parseDateTimeBestEffort(purchased_at)) AS day,
-    toDayOfWeek(parseDateTimeBestEffort(purchased_at)) AS day_of_week,
-    toQuarter(parseDateTimeBestEffort(purchased_at)) AS quarter
+    DATE(purchased_at) AS date_id,
+    YEAR(purchased_at) AS year,
+    MONTH(purchased_at) AS month,
+    DAY(purchased_at) AS day,
+    DAYOFWEEK(purchased_at) AS day_of_week,
+    QUARTER(purchased_at) AS quarter
 FROM {{ ref('stg_orders') }}
 WHERE purchased_at IS NOT NULL
--- ecommerce_analytics/models/fact/fact_orders.sql
+++ ecommerce_analytics/models/fact/fact_orders.sql
@@ -5,18 +5,15 @@ SELECT
     oi.product_id,
     oi.seller_id,
     o.customer_id,
-    toDate(parseDateTimeBestEffort(o.purchased_at)) AS date_id,
+    DATE(o.purchased_at) AS date_id,
     o.status,
     oi.price AS item_price,
-    oi.freight_value AS freight_value,
+    oi.freight_value,
     p.payment_type,
     p.installments,
     p.payment_value AS total_payment,
     r.score AS review_score,
-    dateDiff('day',
-        parseDateTimeBestEffortOrNull(o.purchased_at),
-        parseDateTimeBestEffortOrNull(nullIf(o.delivered_customer_date, ''))
-    ) AS delivery_days
+    DATEDIFF('day', o.purchased_at, o.delivered_customer_date) AS delivery_days
 FROM {{ ref('stg_order_items') }} oi
 JOIN {{ ref('stg_orders') }} o ON o.order_id = oi.order_id
 JOIN {{ ref('stg_order_payments') }} p ON p.order_id = oi.order_id
